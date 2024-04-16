95eaef602c8df16a2382e2e2dd9bdff25d83f25f
Luiz Guilherme Cunha Facundo <luizg5913@gmail.com>
2024-04-16 16:45:42 -0300

    Colocando o tipo da coluna varchar

---
+++ CleanArchMvc.Infra.Data/EntitiesConfig/CategoryConfig.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfig
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //PF
+            builder.HasKey(t => t.Id);
+
+            builder.Property(p => p.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+                    );
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfig/ProductConfig.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfig
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            //PK
+            builder.HasKey(t => t.Id);
+
+            builder.Property(p => p.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasColumnType("varchar")
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.Image)
+                .HasColumnType("varchar")
+                .HasMaxLength(250);
+
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(e => e.Category).WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
